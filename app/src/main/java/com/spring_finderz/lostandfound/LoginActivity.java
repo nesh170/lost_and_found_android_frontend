affc7fe4d89f133aff923e56310f874de9ec3367
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-10-22 14:16:20 -0400

    initial build

---
+++ app/src/main/java/com/spring_finderz/lostandfound/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.spring_finderz.lostandfound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
